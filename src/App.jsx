675284784d1d93a769bac808b2e46093d15bfe13
nvhungw <nvhung2212.w@gmail.com>
2023-07-11 23:48:18 +0700

    git add

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Expand from './components/Expand';
+
+function App() {
+  return (
+    <>
+    <Expand/>
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/Expand.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Expand() {
+
+    const [Open, setOpen] = React.useState(false)
+
+    return(
+        <>
+            <h1>Conditional Rendering</h1>
+            <button type='button' onClick={() => {setOpen(!Open)}}>
+                {Open ? 'Close' : 'Open'}
+            </button>
+            {Open && <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>}
+        </>
+    )
+}
+
+export default Expand
